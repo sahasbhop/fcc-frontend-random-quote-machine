98677c77390f2a3746438bfaa79d278688c1d6c0
Terng Sahasbhop <sahasbhop.suvadhanabhakdi@scbtechx.io>
2022-03-10 00:14:16 +0700

    Complete tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import {useEffect, useState} from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    const [quotes, setQuotes] = useState([])
+    const [quote, setQuote] = useState({text: "n/a", author: "n/a"})
+
+    useEffect(() => {
+        fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+            .then((response) => response.json())
+            .then((json) => {
+                const quotes = json.quotes
+                setQuotes(quotes)
+                newQuotes(quotes)
+            })
+    }, [])
+
+    const newQuotes = (quotes) => {
+        const random = Math.floor(Math.random() * quotes.length)
+        const quote = quotes[random]
+        setQuote({
+            text: quote.quote,
+            author: quote.author
+        })
+    }
+
+    return (
+        <div className="row justify-content-center">
+            <div id="quote-box" className="card text-light bg-dark w-50">
+                <p id="text" className="text-center">"{quote.text}"</p>
+                <p id="author" className="text-end">- {quote.author} -</p>
+                <button id="new-quote"
+                        className="btn btn-primary"
+                        onClick={() => newQuotes(quotes)}>New Quote
+                </button>
+                <a id="tweet-quote"
+                   className="btn btn-secondary"
+                   href="https://twitter.com/intent/tweet"
+                   target="_blank"
+                   rel="noreferrer">Tweet Quote
+                </a>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
